a2efa4d0a9c55bcde5cafe5d0b176835bb2e10d5
Nate Abele <nate.abele@gmail.com>
2011-08-31 06:24:42 +0800

    Fixing whitespace and formatting. Refactoring `Rules` adapter to eliminate static defs, and adding array- and pattern-based IP matching. Fixing docblocks, cleaning up tests.

--- tests/cases/security/AccessTest.php
+++ tests/cases/security/AccessTest.php
@@ -9,63 +9,67 @@
 
 namespace li3_access\tests\cases\security;
 
-use \li3_access\security\Access;
-use \lithium\net\http\Request;
+use li3_access\security\Access;
+use lithium\net\http\Request;
 
 class AccessTest extends \lithium\test\Unit {
 
-    public function setUp() {
-        Access::config(array(
-            'test_access' => array(
-                'adapter' => 'Simple'
-            ),
-            'test_access_with_filters' => array(
-                'adapter' => 'Simple',
-                'filters' => array(
-                    function($self, $params, $chain) {
-                        return $chain->next($self, $params, $chain);
-                    },
-                    function($self, $params, $chain) {
-                        if (!$params['user']) {
-                            return array('message' => 'Access denied.', 'redirect' => $params['options']['redirect']);
-                        } else {
-                            return $chain->next($self, $params, $chain);
-                        }
-                    }
-                )
-            )
-        ));
-    }
+	public function setUp() {
+		Access::config(array(
+			'test_access' => array('adapter' => 'Simple'),
+			'test_access_with_filters' => array(
+				'adapter' => 'Simple',
+				'filters' => array(
+					function($self, $params, $chain) {
+						return $chain->next($self, $params, $chain);
+					},
+					function($self, $params, $chain) {
+						if (!$params['user']) {
+							return array(
+								'message' => 'Access denied.',
+								'redirect' => $params['options']['redirect']
+							);
+						}
+						return $chain->next($self, $params, $chain);
+					}
+				)
+			)
+		));
+	}
 
-    public function tearDown() {}
+	public function tearDown() {}
 
-    public function testCheck() {
-        $request = new Request();
+	public function testCheck() {
+		$request = new Request();
 
-        $expected = array();
-        $result = Access::check('test_access', array('username' => 'Tom'), $request);
-        $this->assertEqual($expected, $result);
+		$expected = array();
+		$result = Access::check('test_access', array('username' => 'Tom'), $request);
+		$this->assertEqual($expected, $result);
 
-        $expected = array('message' => 'Access denied.', 'redirect' => '/login');
-        $result = Access::check('test_access', false, $request, array('redirect' => '/login', 'message' => 'Access denied.'));
-        $this->assertEqual($expected, $result);
-    }
+		$expected = array('message' => 'Access denied.', 'redirect' => '/login');
+		$result = Access::check('test_access', false, $request, array(
+			'redirect' => '/login',
+			'message' => 'Access denied.'
+		));
+		$this->assertEqual($expected, $result);
+	}
 
-    public function testFilters() {
-        $request = new Request();
+	public function testFilters() {
+		$request = new Request();
 
-        $expected = array('message' => 'Access denied.', 'redirect' => '/login');
-        $result = Access::check('test_access_with_filters', false, $request, array('redirect' => '/login'));
-        $this->assertEqual($expected, $result);
-    }
+		$expected = array('message' => 'Access denied.', 'redirect' => '/login');
+		$result = Access::check('test_access_with_filters', false, $request, array(
+			'redirect' => '/login'
+		));
+		$this->assertEqual($expected, $result);
+	}
 
-    public function testNoConfigurations() {
-        $request = new Request();
-
-        Access::reset();
-        $this->assertIdentical(array(), Access::config());
-        $this->expectException("Configuration `test_no_config` has not been defined.");
-        Access::check('test_no_config', false, $request);
-    }
+	public function testNoConfigurations() {
+		Access::reset();
+		$this->assertIdentical(array(), Access::config());
+		$this->expectException("Configuration `test_no_config` has not been defined.");
+		Access::check('test_no_config', false, new Request());
+	}
 }
+
 ?>
