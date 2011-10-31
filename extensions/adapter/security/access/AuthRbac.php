cae787c5b588e702bd269fc56406f4d6032c7835
dgAlien <pti.ms@gmx.net>
2011-10-31 21:24:55 +0100

    fixed Inflector call

--- extensions/adapter/security/access/AuthRbac.php
+++ extensions/adapter/security/access/AuthRbac.php
@@ -4,6 +4,7 @@ namespace li3_access\extensions\adapter\security\access;
 
 use lithium\security\Auth;
 use lithium\core\ConfigException;
+use lithium\util\Inflector;
 
 use li3_access\security\Access;
 
@@ -114,11 +115,11 @@ class AuthRbac extends \lithium\core\Object {
             }
 
             if ($type === 'controller') {
-                $value = \lithium\util\Inflector::underscore($value);
+                $value = Inflector::underscore($value);
             }
 
             if (!array_key_exists($type, $request->params) ||
-				$value !== \lithium\util\Inflector::underscore($request->params[$type])) {
+				$value !== Inflector::underscore($request->params[$type])) {
                 return false;
             }
         }
